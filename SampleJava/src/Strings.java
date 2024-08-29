44654d55f0d2210f367fd5d996640b5501a05f6c
Obsqura Trainer <Obsqura Trainer@DESKTOP-C43945V>
2024-08-29 10:07:21 +0530

    hloi

---
+++ SampleJava/src/Strings.java
@@ -0,0 +1,17 @@
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("Hello");  
+		sb.replace(1,3,"Java");  
+		System.out.println(sb);//prints HJavalo  
+
+   
+		}  
+ 
+
+
+	
+
+}
